3c8bb714e3e7c20055dabbfd43c61b47d3a4c3bc
Andrej-cyber <andvit94@gmail.com>
2021-07-01 14:36:07 +0200

    Add billing and client side of app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+    <h1>
+    Beinar Solutions B.V.
+    </h1>
+    Collect feedback from users
+    </div>
+  )
+};
+
+export default Landing;
--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,14 @@ passport.use(
   callbackURL: '/auth/google/callback',
   proxy: true
   },
-  (accessToken, refreshToken, profile, done) => {
-   User.findOne({ googleId: profile.id })
-  .then(existingUser => {
-    if (existingUser) {
-      //We already have a reccord with the given user profile ID
-      done(null, existingUser);
-  } else {
-      //We do not have a user reccord with this ID, make a new reccord
-      new User({ googleId: profile.id }).save()
-      .then(user => done(null, user));
-       }
-     })
-    }
-  )
+    async (accessToken, refreshToken, profile, done) => {
+         const existingUser = await User.findOne({ googleId: profile.id })
+         if (existingUser) {
+         return done(null, existingUser);
+         }
+
+         const user = await new User({ googleId: profile.id }).save()
+         done(null, user);
+        }
+    )
 );
